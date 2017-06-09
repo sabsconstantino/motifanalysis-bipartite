86afc46396f022294109fb00e4baef240d8abea1
Isabel Constantino <i.m.constantino@se16.qmul.ac.uk>
2017-06-09 22:31:17 +0100

    Added print to track progress

--- bip_counter.py
+++ bip_counter.py
@@ -43,6 +43,7 @@ def count_motifs(B, nodes_U=None, nodes_O=None):
 
     # finding motifs 0, 2, and 4
     for u in nodes_U.keys():
+    	print 'user ' + u + ' ' + str(nodes_U[u])
         if k_U[u] >= 2:
             objs = [o for o in nodes_O.keys() if Ba[nodes_U[u],nodes_O[o]]==1]
             pairs = list(it.combinations(objs,2))
@@ -55,6 +56,7 @@ def count_motifs(B, nodes_U=None, nodes_O=None):
 
     # finding motifs 1, 3, and 5
     for o in nodes_O.keys():
+    	print 'object ' + o + ' ' + str(nodes_O[o])
         if k_O[o] >= 2:
             usr = [u for u in nodes_U.keys() if Ba[nodes_U[u],nodes_O[o]]==1]
             pairs = list(it.combinations(usr,2))
