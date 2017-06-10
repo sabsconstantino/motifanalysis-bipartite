4a36d7c5c375e07b6f0fd2d1f3c9663a51643ab3
Isabel Constantino <i.m.constantino@se16.qmul.ac.uk>
2017-06-10 21:31:02 +0100

    Added counter results and analyses for 2009 and 2010 music data

--- music_2011.py
+++ music_2011.py
@@ -5,32 +5,30 @@ import bip_counter as bc
 from collections import Counter
 import matplotlib.pyplot as plt
 
-B = nx.from_pandas_dataframe(pp.df_music_HR_09,source='user',target='product')
+B = nx.from_pandas_dataframe(pp.df_music_HR_11,source='user',target='product')
 
-nodes_U = list(pp.df_music_HR_09['user'].unique()) # len(nodes_U): 19024
-nodes_O = list(pp.df_music_HR_09['product'].unique()) # len(nodes_O): 11695
+nodes_U = list(pp.df_music_HR_11['user'].unique())
+nodes_O = list(pp.df_music_HR_11['product'].unique())
 
 #---------------------------------------------------------------------
 # motif counting
-'''
+
 motifs = bc.count_motifs(B,nodes_U=nodes_U,nodes_O=nodes_O)
 
 print motifs 
-# [  3.11630000e+04   4.78968000e+05   5.92180103e+08   5.60041815e+09   1.36400000e+03   3.15459000e+05]
-'''
 
 #---------------------------------------------------------------------
 # exploratory graph analysis
 
 # no. of edges
-K = len(B.edges()) # 24676
+K = len(B.edges())
 
 # degrees
 k_U,k_O = nx.bipartite.degrees(B,nodes_O)
 
 # average degree
-avg_kU = sum(k_U.values()) / (len(nodes_U)*1.00) # 1.297098402018503
-avg_kO = sum(k_O.values()) / (len(nodes_O)*1.00) # 2.109961522017956
+avg_kU = sum(k_U.values()) / (len(nodes_U)*1.00)
+avg_kO = sum(k_O.values()) / (len(nodes_O)*1.00)
 
 # degree distribution
 kcount_U = dict(Counter(k_U.values()))
@@ -44,11 +42,17 @@ plt.hold(False)
 plt.loglog(kdist_U.keys(), kdist_U.values(),c='b')
 plt.xlabel("k")
 plt.ylabel("P(k)")
-plt.savefig('plots/pk_music2009_u.png')
+plt.savefig('plots/pk_music2011_u.png')
 
 # plotting degree distribution of users
 plt.hold(False)
 plt.loglog(kdist_O.keys(), kdist_O.values(),c='r')
 plt.xlabel("k")
 plt.ylabel("P(k)")
-plt.savefig('plots/pk_music2009_o.png')
+plt.savefig('plots/pk_music2011_o.png')
+
+print K
+print len(nodes_U)
+print len(nodes_O)
+print avg_kU
+print avg_kO
