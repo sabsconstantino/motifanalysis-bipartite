736dbc6e5dac79558bb0e0d29cb0bd995ca9a77a
Isabel Constantino <i.m.constantino@se16.qmul.ac.uk>
2017-06-13 16:27:55 +0100

    Added preprocessing for video game data, divided per year.

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
+#-----------------------------------------------------------------------------
+# music
+
 df_music = pd.read_csv("data/ratings_music_videogames/ratings_Digital_Music.csv", header=None, names=['user','product','rating','timestamp'])
 
 # complete dataset
@@ -15,23 +18,58 @@ df_music_HR = df_music[df_music['rating'] >= 4]
 # 93157 users, 80851 products
 # 142397 rows
 df_music_HR_14 = df_music_HR[df_music_HR['timestamp'].dt.year == 2014]
+df_music_HR_14 = df_music_HR_14[[0,1]]
 
 # 157863 users, 135429 products
 # 258755 rows
 df_music_HR_13 = df_music_HR[df_music_HR['timestamp'].dt.year == 2013]
+df_music_HR_13 = df_music_HR_13[[0,1]]
 
 # 70641 users, 61469 products
 # 101556 rows
 df_music_HR_12 = df_music_HR[df_music_HR['timestamp'].dt.year == 2012]
+df_music_HR_12 = df_music_HR_12[[0,1]]
 
 # 23313 users, 17272 products
 # 30764 rows
 df_music_HR_11 = df_music_HR[df_music_HR['timestamp'].dt.year == 2011]
+df_music_HR_11 = df_music_HR_11[[0,1]]
 
 # 18642 users, 13001 products
 # 23830 rows
 df_music_HR_10 = df_music_HR[df_music_HR['timestamp'].dt.year == 2010]
+df_music_HR_10 = df_music_HR_10[[0,1]]
 
 # 19024 users, 11695 products
 # 24676 rows
 df_music_HR_09 = df_music_HR[df_music_HR['timestamp'].dt.year == 2009]
+df_music_HR_09 = df_music_HR_09[[0,1]]
+
+#-----------------------------------------------------------------------------
+# video games
+
+df_vg = pd.read_csv("data/ratings_music_videogames/ratings_Video_Games.csv", header=None, names=['user','product','rating','timestamp'])
+
+# complete dataset
+df_vg['timestamp'] = pd.to_datetime(df_vg['timestamp'],unit='s') 
+
+# high rating
+df_vg_HR = df_vg[df_vg['rating'] >= 4]
+
+df_vg_HR_14 = df_vg_HR[df_vg_HR['timestamp'].dt.year == 2014]
+df_vg_HR_14 = df_vg_HR_14[[0,1]]
+
+df_vg_HR_13 = df_vg_HR[df_vg_HR['timestamp'].dt.year == 2013]
+df_vg_HR_13 = df_vg_HR_13[[0,1]]
+
+df_vg_HR_12 = df_vg_HR[df_vg_HR['timestamp'].dt.year == 2012]
+df_vg_HR_12 = df_vg_HR_12[[0,1]]
+
+df_vg_HR_11 = df_vg_HR[df_vg_HR['timestamp'].dt.year == 2011]
+df_vg_HR_11 = df_vg_HR_11[[0,1]]
+
+df_vg_HR_10 = df_vg_HR[df_vg_HR['timestamp'].dt.year == 2010]
+df_vg_HR_10 = df_vg_HR_10[[0,1]]
+
+df_vg_HR_09 = df_vg_HR[df_vg_HR['timestamp'].dt.year == 2009]
+df_vg_HR_09 = df_vg_HR_09[[0,1]]
