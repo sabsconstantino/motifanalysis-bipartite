51514a7e7224278688b4de203644f1d121a94192
Isabel Constantino <i.m.constantino@se16.qmul.ac.uk>
2017-06-22 19:47:10 +0100

    Revised counting for u-o-u-o subgraph

--- bip_counter.py
+++ bip_counter.py
@@ -14,8 +14,8 @@ def count_motifs(B, nodes_U=None, nodes_O=None):
         Returns
         - motifs[0]: o-u-o
         - motifs[1]: u-o-u
-        - motifs[2]: square
-        - motifs[3]: o-u-o-u / u-o-u-o
+        - motifs[2]: o-u-o-u / u-o-u-o
+        - motifs[3]: square
     """
     motifs = np.zeros(4)
 
@@ -23,35 +23,32 @@ def count_motifs(B, nodes_U=None, nodes_O=None):
         nodes_U = [u for u,d in B.nodes_iter(data=True) if d['bipartite']==0]
         nodes_O = [o for o,d in B.nodes_iter(data=True) if d['bipartite']==1]
 
-    num_U = len(nodes_U)
-    num_O = len(nodes_O)
-
-    nodes_U.sort()
-    nodes_O.sort()
-
     k_U,k_O = nx.bipartite.degrees(B,nodes_O)
 
-    # finding motifs 0, 2, and 4
+    # finding motifs 0, 2, and 3
     for u in nodes_U:
         if k_U[u] >= 2:
             objs = B.neighbors(u)
             pairs = list(it.combinations(objs,2))
             motifs[0] += len(pairs)
             for p in pairs:
+                # checking all other neighbours of u
+                #uncommon_user = [j for j in B.neighbors(p[0]) if j != u]
+                #uncommon_user.extend([j for j in B.neighbors(p[1]) if j != u])
+                #motifs[2] += len(uncommon_user)
+
                 # if the two objects have a common user different from u
                 common_user = [j for j in B.neighbors(p[0]) if j in B.neighbors(p[1]) and j != u]
-                motifs[2] += len(common_user)
+                motifs[3] += len(common_user)
+                motifs[2] += ( k_O[p[0]] + k_O[p[1]] - 2*(1 + len(common_user)) )
 
-    # finding motifs 1, 3, and 5
+    # finding motif 1
     for o in nodes_O:
         if k_O[o] >= 2:
             usr = B.neighbors(o)
             pairs = list(it.combinations(usr,2))
             motifs[1] += len(pairs)
-            for p in pairs:
-                motifs[3] += k_U[p[0]] + k_U[p[1]] - 2
 
-    motifs[2] = motifs[2]/2
-    motifs[3] = ( motifs[3] - motifs[2] ) / 2
+    motifs[3] = motifs[3]/2
 
     return motifs
