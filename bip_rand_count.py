ec5774304c106d36e4ef51645398dedd98b2a7e8
Isabel Constantino <i.m.constantino@se16.qmul.ac.uk>
2017-06-24 14:07:02 +0100

    Fixed get_zscores to get only zscores of original data

--- bip_rand_count.py
+++ bip_rand_count.py
@@ -1,12 +1,10 @@
 import networkx as nx
-import preprocessing_cumulative as pc
 import random
 from collections import Counter
 import bip_counter as bc
 import numpy as np
-from scipy import stats
 
-# This module is generating samples from ensembles of random graphs, and for computing the zscores of the motif counts.
+# This module is generating samples from ensembles of random graphs, and for computing the zscores of the subgraph counts.
 
 def rand_samedegrees(df, col1, col2):
     nodes_U = list(df[col1])
@@ -16,10 +14,11 @@ def rand_samedegrees(df, col1, col2):
     ensemble_sample = []    
 
     while len(ensemble_sample) < 100:
+        print len(ensemble_sample)
         sU = list(nodes_U)
         sO = list(nodes_O)
         edges = []
-        while sO:
+        while sO and sU:
             random.shuffle(sU)
             if (sU[-1],sO[-1]) not in edges:
                 edges.append((sU.pop(), sO.pop()))
@@ -27,19 +26,22 @@ def rand_samedegrees(df, col1, col2):
         R.add_edges_from(edges)
         ensemble_sample.append(R) 
 
-    return R   
+    return ensemble_sample  
 
-def get_zscore(R,nodes_O=None,nodes_U=None)
+def get_zscores(count_from_data,rand_ensemble,nodes_U=None,nodes_O=None):
     if (nodes_U==None and nodes_O==None):
         nodes_U = [u for u,d in B.nodes_iter(data=True) if d['bipartite']==0]
         nodes_O = [o for o,d in B.nodes_iter(data=True) if d['bipartite']==1]
 
-    counts = np.zeros([len(ensemble_sample),4],dtype=int)
-    for i in np.arange(len(ensemble_sample)):
-        counts[i] = bc.count_motifs(R, nodes_U, nodes_O)    
+    ensemble_counts = np.zeros([len(rand_ensemble),4],dtype=int)
+    for i in np.arange(len(ensemble)):
+        print i
+        ensemble_counts[i] = bc.count_subgraphs(rand_ensemble[i], nodes_U, nodes_O)    
 
-    zscores = np.zeros([len(ensemble_sample),4])
-    for i in np.arange(4):
-    	zscores[:,i] = stats.zscore(counts[:,i])
+    zscores = np.zeros(4,dtype=np.float64)
+    mjus = np.mean(ensemble_counts,axis=0)
+    sigmas = np.std(ensemble_counts,axis=0)
+
+    zscores = (count_from_data - mjus) / sigmas
 
     return zscores
