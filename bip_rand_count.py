8358d4c09fc3c252a83333b38f331f55d7465a43
Isabel Constantino <i.m.constantino@se16.qmul.ac.uk>
2017-07-06 00:21:34 +0100

    rand_samedegrees: added parameter pct_cap to denote how many percent of the edges to add to the initial random graph

--- bip_rand_count.py
+++ bip_rand_count.py
@@ -6,7 +6,7 @@ import numpy as np
 
 # This module is generating samples from ensembles of random graphs, and for computing the zscores of the subgraph counts.
 
-def rand_samedegrees(df, col1, col2, fname, fname_start=0, fname_end=100):
+def rand_samedegrees(df, col1, col2, fname, pct_cap=1, fname_start=0, fname_end=100):
     nodes_U = list(df[col1].unique())
     nodes_O = list(df[col2].unique())
 
@@ -14,49 +14,43 @@ def rand_samedegrees(df, col1, col2, fname, fname_start=0, fname_end=100):
     stubs_O = list(df[col2])
     stubs_O.sort(key=Counter(stubs_O).get,reverse=False)
 
-    rej_threshold = int(len(stubs_O) * 0.03)
     num_graphs = 0
 
     while num_graphs < (fname_end - fname_start):
         reject_graph = False
         print num_graphs
         random.shuffle(stubs_U)
-        init_edges = zip(stubs_O,stubs_U)
+        init_cap = int(len(stubs_U)*pct_cap) # number of edges to be added initially
+        init_edges = zip(stubs_O[0:init_cap],stubs_U[0:init_cap])
         R = nx.Graph()
         R.add_edges_from(init_edges)
-        rej_ct = len(stubs_O) - len(R.edges())
-        if rej_ct > rej_threshold:
-            reject_graph = True
-        else:
-            # get remaining stubs
-            k_Uin,k_Oin = nx.bipartite.degrees(R,nodes_O)
-            remaining_U = list((Counter(stubs_U)-Counter(k_Uin)).elements())
-            remaining_O = list((Counter(stubs_O)-Counter(k_Oin)).elements())
-            remaining_O.sort(key=Counter(remaining_O).get,reverse=False)
+        
+        # get remaining stubs
+        k_Uin,k_Oin = nx.bipartite.degrees(R,nodes_O)
+        remaining_U = list((Counter(stubs_U)-Counter(k_Uin)).elements())
+        remaining_O = list((Counter(stubs_O)-Counter(k_Oin)).elements())
+        remaining_O.sort(key=Counter(remaining_O).get,reverse=False)
 
-            random.shuffle(remaining_U)
-            current_rej = 0
-            while remaining_O and remaining_U:
-                print len(remaining_O)
-                prev_edgect = len(R.edges())
-                R.add_edge(remaining_U[-1],remaining_O[-1])
-                if prev_edgect < len(R.edges()): # if adding the edge was successful; i.e. the edge is not yet in the graph
-                    remaining_U.pop()
-                    remaining_O.pop()
-                    current_rej = 0
-                else:
-                    random.shuffle(remaining_U)
-                    rej_ct += 1
-                    current_rej += 1
-                    print 'total rejections: ' + str(rej_ct) + ' and current rejections: ' + str(current_rej)
-                if rej_ct > rej_threshold or current_rej > 100 or (len(remaining_U) == 1 and current_rej > 1):
-                    reject_graph = True
-                    break
+        random.shuffle(remaining_U)
+        current_rej = 0
+        while remaining_O and remaining_U:
+            print len(remaining_O)
+            prev_edgect = len(R.edges())
+            R.add_edge(remaining_U[-1],remaining_O[-1])
+            if prev_edgect < len(R.edges()): # if adding the edge was successful; i.e. the edge is not yet in the graph
+                del remaining_U[-1]
+                del remaining_O[-1]
+                current_rej = 0
+            else:
+                random.shuffle(remaining_U)
+                current_rej += 1
+            if current_rej > 100 or (len(remaining_U) == 1 and current_rej > 1):
+                reject_graph = True
+                break
 
         if reject_graph == False:
             nx.write_gml(R,fname + str(num_graphs + fname_start) + ".gml")
             num_graphs += 1
-
 
 def rand_sameU_randO(df, col1, col2, fname_start=0, fname_end=100):
     stubs_U = list(df[col1])
