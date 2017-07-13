05eb975957be5cc662f44f462b8f2af6e607c4ec
Isabel Constantino <i.m.constantino@se16.qmul.ac.uk>
2017-07-13 02:09:05 +0100

    Added test case for 5-node subgraphs

--- test.py
+++ test.py
@@ -14,9 +14,14 @@ edges = [(0,10),(0,11),(1,10),(1,12),(2,12),(2,13),(3,14),(4,12),(4,13),(4,15),(
 
 B1.add_edges_from(edges)
 
-print(bc.count_motifs(B1))
+print(bc.count_subgraphs(B1))
 
 # example 2
 B2 = nx.bipartite.gnmk_random_graph(500,300,1350)
 
-print(bc.count_motifs(B2))
+#print(bc.count_subgraphs(B2))
+
+edges2 = [(0,'a'),(0,'b'),(1,'a'),(1,'b'),(1,'c'),(2,'a'),(2,'b'),(2,'c'),(2,'d'),(3,'c')]
+B3 = nx.Graph()
+B3.add_edges_from(edges2)
+print(bc.count_subgraphs(B3,[0,1,2,3],['a','b','c','d']))
