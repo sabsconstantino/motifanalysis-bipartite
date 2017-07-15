f64bfae45d2cf3a7a3815454d85efd501a48b31e
Isabel Constantino <i.m.constantino@se16.qmul.ac.uk>
2017-07-15 01:14:11 +0100

    Used defaultdict instead of Counter due to performance

--- bip_counter.py
+++ bip_counter.py
@@ -2,7 +2,8 @@ import networkx as nx
 import numpy as np
 import itertools as it
 import scipy.sparse as ssp
-from collections import Counter
+from collections import Counter, defaultdict
+from datetime import datetime as dt
 
 def count_subgraphs(B, nodes_U=None, nodes_O=None):
     """Counts subgraphs of a bipartite user-object graph
@@ -25,81 +26,70 @@ def count_subgraphs(B, nodes_U=None, nodes_O=None):
     subgraphs = np.zeros(8,dtype=np.int64)
 
     if (nodes_U==None and nodes_O==None):
-        nodes_U = [u for u,d in B.nodes_iter(data=True) if d['bipartite']==0]
-        nodes_O = [o for o,d in B.nodes_iter(data=True) if d['bipartite']==1]
+        nodes_U = [u for u,d in B.nodes_iter(data=True) if d['bipartite']==0 or d['bipartite']=='u']
+        nodes_O = [o for o,d in B.nodes_iter(data=True) if d['bipartite']==1 or d['bipartite']=='o']
 
     k_U,k_O = nx.bipartite.degrees(B,nodes_O)
     num_U = len(nodes_U)
 
-    pairs_O = []
+    nodes_U.sort(key=k_U.get, reverse=False)
+    nodes_O.sort(key=k_O.get, reverse=False)
+
+    # https://stackoverflow.com/questions/27801945/surprising-results-with-python-timeit-counter-vs-defaultdict-vs-dict
+
+    # pairs_O = []
+    # pairs_U = []
+    ct_pairs_O = defaultdict(int)
+    ct_pairs_U = defaultdict(int)
+
+    #print 'hey ' + str(dt.now()) 
     for u in nodes_U:
         if k_U[u] >= 2:
             objs = B.neighbors(u)
-            pairs_O.extend(list(it.combinations(objs,2)))
+            objs.sort()
+            pairs_O = it.combinations(objs,2)
+            for p in pairs_O:
+                ct_pairs_O[p] += 1
 
-    pairs_U = []
+    #print 'ho ' + str(dt.now())
     for o in nodes_O:
         if k_O[o] >= 2:
             usr = B.neighbors(o)
-            pairs_U.extend(list(it.combinations(usr,2)))
+            usr.sort()
+            pairs_U = it.combinations(usr,2)
+            for p in pairs_U:
+                ct_pairs_U[p] += 1
 
-    pairs_U.sort(key = lambda x: x[0])
-    pairs_O.sort(key = lambda x: x[0])
+    #print 'hello ' + str(dt.now())
+    # subgraphs[0] = len(pairs_O)
+    # subgraphs[1] = len(pairs_U)
 
-    ct_pairs_U = Counter(pairs_U)
-    ct_pairs_O = Counter(pairs_O)
+    del nodes_O
+    del nodes_U
 
-    subgraphs[0] = len(pairs_O)
-    subgraphs[1] = len(pairs_U)
+    subgraphs[0] = sum(ct_pairs_O.values())
+    subgraphs[1] = sum(ct_pairs_U.values())
 
+    # ct_pairs_O = Counter(pairs_O)
+    # ct_pairs_U = Counter(pairs_U)
+
+    #print 'are we there yet ' + str(len(ct_pairs_O.keys())) +' '+ str(dt.now())
     for p in ct_pairs_O.keys():
         subgraphs[2] += num_U - ( k_O[p[0]] + k_O[p[1]] - ct_pairs_O[p] )
-        subgraphs[3] += ct_pairs_O[p]*( k_O[p[0]] + k_O[p[1]] - 2 )
-        if ct_pairs_O[p] > 1:
+        if ct_pairs_O[p] <= 1:
+            subgraphs[3] += k_O[p[0]] + k_O[p[1]] - 2 
+        else:
             squares = ct_pairs_O[p]*(ct_pairs_O[p]-1)/2
             subgraphs[4] += squares
             subgraphs[5] += squares*len(set(B.neighbors(p[0])).symmetric_difference(set(B.neighbors(p[1]))))
             subgraphs[7] += ct_pairs_O[p]*(ct_pairs_O[p]-1)*(ct_pairs_O[p]-2) / 6
+        del ct_pairs_O[p]
 
+    #print 'almost there ' + str(len(ct_pairs_U.keys())) +' '+ str(dt.now())
     for p in ct_pairs_U.keys():
         if ct_pairs_U[p] > 1:
             squares = ct_pairs_U[p]*(ct_pairs_U[p]-1)/2
             subgraphs[6] += squares*len(set(B.neighbors(p[0])).symmetric_difference(set(B.neighbors(p[1]))))
+        del ct_pairs_U[p]
             
-
-    subgraphs[3] -= 3*subgraphs[4]
-
-    # # finding subgraphs 0, 2, and 3
-    # for u in nodes_U:
-    #     if k_U[u] >= 2:
-    #         objs = B.neighbors(u)
-    #         pairs = list(it.combinations(objs,2))
-    #         subgraphs[0] += len(pairs)
-    #         for p in pairs:
-    #             subgraphs[3] += ( k_O[p[0]] + k_O[p[1]] - 2 )
-    #             # if the pairs of objects have a common user other than the current one
-    #             common_user = len(set(B.neighbors(p[0])).intersection(set(B.neighbors(p[1]))))
-    #             subgraphs[4] += (common_user - 1)
-    #             if common_user > 1:
-    #                 subgraphs[5] += ( k_O[p[0]] + k_O[p[1]] - 4 )
-    #                 subgraphs[7] += common_user*(common_user-1)*(common_user-2) / 6
-    #             subgraphs[2] += num_U - ( k_O[p[0]] + k_O[p[1]] - common_user ) 
-
-    # # finding motif 1
-    # for o in nodes_O:
-    #     if k_O[o] >= 2:
-    #         usr = B.neighbors(o)
-    #         pairs = list(it.combinations(usr,2))
-    #         subgraphs[1] += len(pairs)
-    #         for p in pairs:
-    #             common_obj = len(set(B.neighbors(p[0])).intersection(set(B.neighbors(p[1]))))
-    #             if common_obj > 1:
-    #                 subgraphs[6] += ( k_U[p[0]] + k_U[p[1]] - 4 )
-
-    # subgraphs[4] = subgraphs[4]/2
-    # subgraphs[3] = subgraphs[3] - 3*subgraphs[4]
-    # subgraphs[5] = subgraphs[5]/2
-    # subgraphs[6] = subgraphs[6]/2
-    # subgraphs[7] = subgraphs[7]/3
-
     return subgraphs
