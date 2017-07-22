beaefe7b21b4ee2eebe9672de8c70cf4edff4467
Isabel Constantino <i.m.constantino@se16.qmul.ac.uk>
2017-07-22 02:34:48 +0100

    Added option to output a csv of individual subgraph counts in get_zscores

--- bip_rand_count.py
+++ bip_rand_count.py
@@ -13,10 +13,6 @@ def rand_samedegrees(df, col1, col2, fname, fname_start=0, fname_end=100):
     stubs_U.sort(key=Counter(stubs_U).get,reverse=False)
     stubs_O.sort(key=Counter(stubs_O).get,reverse=False)
 
-    R = nx.Graph()
-    R.add_nodes_from(set(stubs_U), bipartite='u')
-    R.add_nodes_from(set(stubs_O), bipartite='o')
-    
     nodes_O = Counter(stubs_O).most_common()
 
     num_graphs = 0
@@ -25,6 +21,9 @@ def rand_samedegrees(df, col1, col2, fname, fname_start=0, fname_end=100):
         print num_graphs
         k_U = Counter(stubs_U)
 
+        R = nx.Graph()
+        R.add_nodes_from(set(stubs_U), bipartite='u')
+        R.add_nodes_from(set(stubs_O), bipartite='o')
         edges = []
         for o in nodes_O:
             users = np.random.choice(k_U.keys(),o[1],replace=False)
@@ -43,16 +42,15 @@ def rand_sameU_randO(df, col1, col2, fname, fname_start=0, fname_end=100):
     k_U = Counter(list(df[col1]))
     nodes_U = list(df[col1].unique())
     nodes_O = list(df[col2].unique())
-
-    R = nx.Graph()
-    R.add_nodes_from(nodes_O, bipartite='u')
-    R.add_nodes_from(nodes_U, bipartite='o')
     
     num_graphs = 0
 
     while num_graphs < (fname_end - fname_start):
         print num_graphs
 
+        R = nx.Graph()
+        R.add_nodes_from(nodes_O, bipartite='o')
+        R.add_nodes_from(nodes_U, bipartite='u')
         edges = []
         for u in k_U.keys():
             objects = np.random.choice(nodes_O,k_U[u],replace=False)
@@ -62,7 +60,7 @@ def rand_sameU_randO(df, col1, col2, fname, fname_start=0, fname_end=100):
         nx.write_gml(R,fname + str(num_graphs + fname_start) + ".gml")
         num_graphs += 1
 
-def get_zscores(count_from_data,fname,fname_start=0,fname_end=100,nodes_U=None,nodes_O=None):
+def get_zscores(count_from_data,fname,fname_start=0,fname_end=100,nodes_U=None,nodes_O=None,outfile=None):
     num_subgraphs = len(count_from_data)
     num_rg = fname_end - fname_start
     ensemble_counts = np.zeros([num_rg,num_subgraphs],dtype=np.int64)
@@ -80,5 +78,12 @@ def get_zscores(count_from_data,fname,fname_start=0,fname_end=100,nodes_U=None,n
     sigmas = np.std(ensemble_counts,axis=0)
 
     zscores = (count_from_data - mjus) / sigmas
+
+    if outfile != None:
+        h = ''
+        for i in np.arange(num_subgraphs-1):
+            h += 'subgraph_' + str(i) + ','
+        h += 'subgraph_' + str(num_subgraphs-1)
+        np.savetxt(outfile, ensemble_counts, header=h, delimiter=',')
 
     return zscores
