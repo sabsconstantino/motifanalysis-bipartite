c7c63f6052eb71540f8ae3e505fd8ac8725a8b2b
Isabel Constantino <i.m.constantino@se16.qmul.ac.uk>
2017-08-02 21:59:50 +0100

    Renamed rand_sameU_randO to rand_randomobj. Speed improvements for rand_randomobj. Added docstrings.

--- bip_rand_count.py
+++ bip_rand_count.py
@@ -5,15 +5,24 @@ import bip_counter as bc
 import numpy as np
 from datetime import datetime as dt
 
-# This module is generating samples from ensembles of random graphs, and for computing the zscores of the subgraph counts.
+"""This module is generating samples from ensembles of random graphs, and for computing the zscores of the subgraph counts.
+"""
 
 def rand_samedegrees(df, col1, col2, fname, fname_start=0, fname_end=100):
+    """Generates (fname_end-fname_start) random graphs with the same degrees as the bipartite network represented by df. Outputs compressed gml files.
+
+    Keyword arguments:
+    df -- pandas dataframe containing network data
+    col1 -- column name of bipartite node set U
+    col2 -- column name of bipartite node set O
+    fname -- output file prefix
+    fname_start -- starting number of filename (default 0)
+    fname_end -- ending number of filename (default 100)
+    """
     stubs_U = list(df[col1])
     stubs_O = list(df[col2])
-    stubs_U.sort(key=Counter(stubs_U).get,reverse=False)
-    stubs_O.sort(key=Counter(stubs_O).get,reverse=False)
 
-    nodes_O = Counter(stubs_O).most_common()
+    k_O = Counter(stubs_O).most_common() # list of node-degree tuples (o, k_o)
 
     num_graphs = 0
 
@@ -25,9 +34,9 @@ def rand_samedegrees(df, col1, col2, fname, fname_start=0, fname_end=100):
         R.add_nodes_from(set(stubs_U), bipartite='u')
         R.add_nodes_from(set(stubs_O), bipartite='o')
         edges = []
-        for o in nodes_O:
+        for o in k_O:
             users = np.random.choice(k_U.keys(),o[1],replace=False)
-            edges.extend(zip( [o[0]]*o[1], users) )
+            edges.extend(zip( [o[0]]*o[1], users ))
             for u in users:
                 if k_U[u] == 1:
                     del k_U[u]
@@ -38,29 +47,62 @@ def rand_samedegrees(df, col1, col2, fname, fname_start=0, fname_end=100):
         nx.write_gml(R,fname + str(num_graphs + fname_start) + ".gml.gz")
         num_graphs += 1
 
-def rand_sameU_randO(df, col1, col2, fname, fname_start=0, fname_end=100):
-    k_U = Counter(list(df[col1]))
+def rand_randomobj(df, col1, col2, fname, fname_start=0, fname_end=100):
+    """Generates (fname_end-fname_start) random graphs with the same degree of U (col1) and randomized degrees of O (col2). Outputs compressed gml files.
+
+    Keyword arguments:
+    df -- pandas dataframe containing network data
+    col1 -- column name of bipartite node set U
+    col2 -- column name of bipartite node set O
+    fname -- output file prefix
+    fname_start -- starting number of filename (default 0)
+    fname_end -- ending number of filename (default 100)
+    """
     nodes_U = list(df[col1].unique())
     nodes_O = list(df[col2].unique())
+    num_O = len(nodes_O)
     
     num_graphs = 0
 
     while num_graphs < (fname_end - fname_start):
         print num_graphs
-
         R = nx.Graph()
         R.add_nodes_from(nodes_O, bipartite='o')
         R.add_nodes_from(nodes_U, bipartite='u')
-        edges = []
-        for u in k_U.keys():
-            objects = np.random.choice(nodes_O,k_U[u],replace=False)
-            edges.extend(zip( [u]*k_U[u], objects) )
 
+        # choose random objects to pair with users
+        stubs_U = list(df[col1])
+        stubs_O = np.random.choice(nodes_O, len(stubs_U), replace=True)
+        edges = zip(stubs_U, stubs_O)
         R.add_edges_from(edges)
+
+        # get remaining stubs (in case some edges were rejected from first round)
+        nodes_Uin = [u for u,d in R.nodes(data=True) if d['bipartite']=='u']
+        stubs_Uin = Counter(nx.bipartite.degrees(R,nodes_Uin)[1])
+        k_U = Counter(stubs_U) - stubs_Uin # counter of remaining stubs from U
+
+        # pair remaining stubs
+        for u in k_U.keys():
+            poss_O = set(nodes_O) - set(R.neighbors(u)) # list of objects we can possibly choose from
+            objects = np.random.choice(list(poss_O), k_U[u], replace=False)
+            R.add_edges_from( zip( [u]*k_U[u], objects ) )
+
         nx.write_gml(R,fname + str(num_graphs + fname_start) + ".gml.gz")
         num_graphs += 1
 
 def get_zscores(count_from_data,fname,is_compressed=True,fname_start=0,fname_end=100,nodes_U=None,nodes_O=None,outfile=None):
+    """Calculates the zscores of the actual data vs. the random graphs.
+
+    Keyword arguments:
+    count_from_data -- a numpy array containing each subgraph count
+    fname -- filename prefix of random graphs to be used
+    is_compressed -- whether the random graph gml files are compressed (default True)
+    fname_start -- starting number of random graph filename (default 0)
+    fname_end -- ending number of random graph filename (default 100)
+    nodes_U -- unique list or set of nodes corresponding to bipartite node set U. This is necessary when there is no 'bipartite' node attribute in the random graph data. (default None)
+    nodes_O -- unique list or set of nodes corresponding to bipartite node set O. This is necessary when there is no 'bipartite' node attribute in the random graph data. (default None)
+    outfile -- file where the subgraph counts of the random graphs are to be written (default None)
+    """
     num_subgraphs = len(count_from_data)
     num_rg = fname_end - fname_start
     ensemble_counts = np.zeros([num_rg,num_subgraphs],dtype=np.int64)
